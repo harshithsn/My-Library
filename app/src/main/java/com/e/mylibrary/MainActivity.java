c28142af851b3e584b07f0bc7c1f35877c7ba0c7
Harshith S Naik <harshithsn1@gmail.com>
2020-08-16 10:53:41 +0530

    MyLibraryApp

--- app/src/main/java/com/e/mylibrary/MainActivity.java
+++ app/src/main/java/com/e/mylibrary/MainActivity.java
@@ -1,12 +1,30 @@
 package com.e.mylibrary;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +32,62 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,Login_Activity.class);
-        startActivity(intent);
-        finish();
+        //Hooks
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        textView=findViewById(R.id.textView);
+        toolbar=findViewById(R.id.toolbar);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        //navigation drawer menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new
+                ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_navigation_drawer,R.string.close_navigation_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(MainActivity.this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: break;
+            case R.id.nav_collection:
+                Intent intent = new Intent(MainActivity.this, MyCollection.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_profile:
+                Intent intent1 = new Intent(MainActivity.this, UserProfile.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_login:
+            case R.id.nav_logout:
+                Intent intent2 = new Intent(MainActivity.this, Login_Activity.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_share: Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show(); break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START); return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
---
+++ app/src/main/java/com/e/mylibrary/MyCollection.java
@@ -0,0 +1,14 @@
+package com.e.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_collection);
+    }
+}
